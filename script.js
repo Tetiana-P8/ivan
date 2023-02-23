9a77fa2f3b7729aca769b74169715fe49854fb5d
Tetiana <potapova.taty.8@gmail.com>
2023-02-23 03:25:55 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+// alert('hello');
+
+// const result = confirm('Are you here?');
+
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', '');
+
+// console.log(answer);
+ 
+// let age = prompt('Сколько тебе лет?', '');
+
+// alert(`Тебе ${age} лет!`);
+
+const category = 'toys'; 
+
+console.log(4 + +'5');
+
+let incr = 10,
+	decr = 10;
+
+// ++incr;
+// --decr;
+
+
+console.log(incr++);
+console.log(decr--);
